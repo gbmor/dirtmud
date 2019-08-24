b1eff49c17fbc4dfa8a20c788f1be94896642b64
Ben Morrison <ben@gbmor.dev>
2019-08-23 23:17:36 -0400

    logging mechanism finished

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,30 @@ use std::{
 
 use chrono::prelude::*;
 
+use log::{
+    info,
+};
+
 mod clients;
 mod engine;
+mod logging;
 
 fn main() -> Result<(), io::Error> {
+    logging::init();
+
     let thetime = Utc::now();
     println!();
     println!("dirtmud 0.1-dev");
-    println!("{}", thetime.to_rfc2822());
 
+    info!("dirtmud 0.1-dev");
+    info!("Startup at: {}", thetime.to_rfc2822());
+
+    info!("Spawning engine worker");
     let (tx, rx) = mpsc::channel::<String>();
     engine::spawn_worker(rx);
 
     // This will block
+    info!("Spawning client worker");
     clients::spawn_worker("0.0.0.0:56543", tx)?;
 
     Ok(())
